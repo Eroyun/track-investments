e9189d334e157bf42f93fdb7fda31852dc4b6c0e
Eren Erol <60065768+Eroyun@users.noreply.github.com>
2024-03-21 16:11:53 +0300

    change file name

--- app/components/addStockModal.js
+++ app/components/addStockModal.js
@@ -13,7 +13,7 @@ import AddIcon from "@mui/icons-material/Add";
 import CloseIcon from "@mui/icons-material/Close";
 import Select from "react-select";
 
-import { addTransaction } from "../helpers/hookHelpers";
+import { addTransaction } from "../helpers/hooks";
 import {
   currencies,
   markets,
