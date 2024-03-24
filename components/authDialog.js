a1697d2876f262954f3a86f3cd00ab403e97e435
Melinda Ghertiu <157207717+melindaGh@users.noreply.github.com>
2024-03-24 16:11:49 +0300

    fix styling

--- components/authDialog.js
+++ components/authDialog.js
@@ -76,7 +76,6 @@ const AuthDialog = () => {
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
-                marginTop: "5rem",
               }}
             >
               <Typography
@@ -84,7 +83,7 @@ const AuthDialog = () => {
                 sx={{
                   fontFamily: '"Segoe UI Emoji"',
                   fontWeight: 700,
-                  color: darkTheme.palette.white.main,
+                  color: darkTheme.palette.white.main, // Use theme color
                   mb: 2,
                 }}
               >
@@ -106,24 +105,6 @@ const AuthDialog = () => {
 
             <div
               className="auth-form-container"
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                height: "60vh",
-                justifyContent: "center",
-                backgroundColor: darkTheme.palette.white.main,
-                boxShadow: "5px 3px 5px rgba(0, 0, 0, 0.2)",
-                marginTop: "5rem",
-                opacity: "0.9",
-                borderRadius: "8px",
-                padding: "2rem",
-                transition: "all 0.3s ease-in-out",
-                width: "50%",
-                maxWidth: "500px",
-                marginLeft: "auto",
-                marginRight: "auto",
-                boxShadow: "10px 5px 5px rgb(0, 187, 252)",
-              }}
             >
               <AuthForm isLogin={isLogin} toggleIsLogin={toggleIsLogin} />
             </div>
